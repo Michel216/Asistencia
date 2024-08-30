c37cb1fe32c27d037e11c48504a7bb500f91c589
Michel216 <micheljasbleidycorreaardila@gmail.com>
2024-08-30 00:51:26 -0500

    Elimina todos los archivos y carpetas del repositorio

--- src/modelos/bitacora.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const bitacoraSchema = new mongoose.Schema({
-  id_aprendiz: { type: mongoose.Schema.Types.ObjectId, ref: 'Aprendiz', required: true },
-  fecha: { type: Date, default: Date.now }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Bitacora', bitacoraSchema);
