2e8c8c2b58012acd8ee1cdabd2b381d2992206ac
Michel216 <micheljasbleidycorreaardila@gmail.com>
2024-09-05 17:47:44 -0500

    Eliminar todos los archivos

--- backend/src/modelos/ficha.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const fichaShema = new mongoose.Schema({
-    codigo: { type: String, require: true, unique: true },
-    nombre: { type: String, require: true },
-    estado: {type: String, default:1}
-}, {timestamps: true});
-
-module.exports = mongoose.model('Ficha', fichaShema)
