3dcd0a7f593b2fa108464b30825eb178d5a6323e
Michel <157923359+Michel216@users.noreply.github.com>
2024-09-06 16:38:04 -0500

    cambios
    Signed-off-by: Michel <157923359+Michel216@users.noreply.github.com>

--- frontend asistencia/src/stores/usuario.js
+++ frontend asistencia/src/stores/usuario.js
@@ -2,7 +2,7 @@ import { ref } from 'vue';
 import { defineStore } from 'pinia';
 import { Notify } from 'quasar';
 import axios from 'axios';
-API_URL= 'https://asistencia-backend-31lj.onrender.com'
+const API_URL= 'https://asistencia-backend-31lj.onrender.com'
 
 // Definición de la tienda para el manejo de usuarios y token
 export const useUsuariosStore = defineStore('usuario', () => {
