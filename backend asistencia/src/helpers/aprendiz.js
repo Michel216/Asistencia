58c0f4a7c83a7fcbd9db5a6ad3dbfb3d3867e640
Michel <157923359+Michel216@users.noreply.github.com>
2024-09-06 15:44:17 -0500

    Cambio
    Signed-off-by: Michel <157923359+Michel216@users.noreply.github.com>

--- backend asistencia/src/helpers/aprendiz.js
+++ backend asistencia/src/helpers/aprendiz.js
@@ -16,20 +16,4 @@ module.exports = {
     validarCCUnico
 }
 
-const Aprendiz = require('../modelos/aprendiz');
 
-const validarCCUnico = async (documento) => {
-    if (!documento || !documento.trim()) {
-        throw new Error('El campo documento no puede ser vacío');
-    }
-
-    const existente = await Aprendiz.findOne({ documento });
-    if (existente) {
-        throw new Error('Ya existe un aprendiz con este documento');
-    }
-};
-
-
-module.exports = {
-    validarCCUnico
-}
