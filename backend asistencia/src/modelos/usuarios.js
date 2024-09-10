3e667c7cdde4bbd836925a7fd06840cc5cd7248a
Michel216 <micheljasbleidycorreaardila@gmail.com>
2024-09-10 15:14:44 -0500

    Cambio

--- backend asistencia/src/modelos/usuarios.js
+++ backend asistencia/src/modelos/usuarios.js
@@ -4,7 +4,7 @@ const usuariosShema = new mongoose.Schema({
     email: { type: String, require: true, unique: true },
     password: { type: String, require: true },
     nombre: { type: String, require: true },
-    estado: {type: Boolean, default:1}
+    estado: {type: String, default:1}
 }, {timestamps: true});
 
 module.exports = mongoose.model('Usuarios', usuariosShema)
