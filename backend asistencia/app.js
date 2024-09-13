8f7c3fd55a360e3ae2aa79db0123bbf3acc99a6a
Michel <157923359+Michel216@users.noreply.github.com>
2024-09-12 19:56:39 -0500

    Update app.js
    Signed-off-by: Michel <157923359+Michel216@users.noreply.github.com>

--- backend asistencia/app.js
+++ backend asistencia/app.js
@@ -15,8 +15,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     'https://asistencia-i7sv.onrender.com',
-     'http://localhost:5174',
-   '*'
+    
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
